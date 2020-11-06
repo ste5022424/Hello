748f285fbd3d61dd4fd375cac61b8ce1b37005d3
I Y <yi9813804@gmail.com>
2020-11-06 17:21:17 +0800

    init

---
+++ Hello/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿< Target Name = "BeforeBuild" >
+
+   < ItemGroup >
+
+     < AssemblyAttributes Include = "AssemblyVersion" >
+
+        < _Parameter1 >$(Version) </ _Parameter1 >
+
+        </ AssemblyAttributes >
+
+      </ ItemGroup >
+
+      < MakeDir Directories = "$(IntermediateOutputPath)" />
+
+       < WriteCodeFragment Language = "C#"
+                     OutputFile = "$(IntermediateOutputPath)Version.cs"
+                     AssemblyAttributes = "@(AssemblyAttributes)" />
+  < ItemGroup >
+    < Compile Include = "$(IntermediateOutputPath)Version.cs" />
+
+   </ ItemGroup >
+ </ Target >
